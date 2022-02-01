c89f17dfd84aa7586998eaac038e56dbb184b8ff
Vasilis <vasilisgoutz08@gmail.com>
2022-02-01 23:28:04 +0200

    Format changes.

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ import aes
 message = "Hello there"
 cipherKey = "as78ax56xty98hfe"
 cipherMessage = aes.encrypt(message, cipherKey)
-print('Cipher Text: ' + cipherMessage)
+print("Cipher Text: " + cipherMessage)
 
 plainMessage = aes.decrypt(cipherMessage, cipherKey)
-print('Plain Text: ' + plainMessage)
+print("Plain Text: " + plainMessage)
 ```
--- aes-test.py
+++ aes-test.py
@@ -1,9 +1,9 @@
 import aes
 
 message = "Hello there"
-cipherKey = 'as78ax56xty98hfe'
+cipherKey = "as78ax56xty98hfe"
 cipherMessage = aes.encrypt(message, cipherKey)
-print('Cipher Text: ' + cipherMessage)
+print("Cipher Text: " + cipherMessage)
 
 plainMessage = aes.decrypt(cipherMessage, cipherKey)
-print('Plain Text: ' + plainMessage)
+print("Plain Text: " + plainMessage)
